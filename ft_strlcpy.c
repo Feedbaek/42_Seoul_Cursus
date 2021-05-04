90975722f8ee0440872eb0c596545b6680ff908e
nicek789 <nicek789@naver.com>
2021-05-04 15:00:54 +0900

    a

---
+++ ft_strlcpy.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: minskim2 <minskim2@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/04 14:25:49 by minskim2          #+#    #+#             */
+/*   Updated: 2021/05/04 15:00:05 by minskim2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+static unsigned int ft_strlen(const char *str)
+{
+	unsigned int len;
+
+	len = 0;
+	while (str[len])
+		len++;
+	return (len);
+}
+
+int *ft_strlcpy(char *dest, const char *src, unsigned int size)
+{
+	unsigned int i;
+	unsigned int len;
+
+	i = 0;
+	len = ft_strlen(src);
+	if (!dest || !src)
+		return (0);
+	while (i < len && i + 1 < size)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (size > 0)
+		dest[i] = 0;
+	return (len);
+}
