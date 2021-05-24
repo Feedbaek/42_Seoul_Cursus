ddc9b3f18b48ef5097b4756feca2074bb9221ecc
minskim2 <nicek789@naver.com>
2021-05-24 11:37:58 +0900

    done

--- get_next_line/get_next_line_bonus.h
+++ get_next_line/get_next_line_bonus.h
@@ -6,7 +6,7 @@
 /*   By: minskim2 <minskim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/10 22:19:22 by minskim2          #+#    #+#             */
-/*   Updated: 2021/05/24 11:18:15 by minskim2         ###   ########.fr       */
+/*   Updated: 2021/05/24 11:37:27 by minskim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,16 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 5000
-# endif
-
-# ifndef OPEN_MAX
-#  define OPEN_MAX 32
-# endif
+# define OPEN_MAX 256
 
 size_t	ft_strlen(char *str);
 int		ft_strchr(char *str);
 char	*ft_strdup(char *s1);
 char	*ft_strjoin(char *s1, char *s2);
+char	*ft_split(char **hi, int n_idx);
+int		WTF_output(char **hi, char **line, int read_size);
+int		get_next_line(int fd, char **line);
+
 int		get_next_line(int fd, char **line);
 
 #endif
