c2cc7341029fb0f050e7478646a3484efb356b81
minskim2 <nicek789@naver.com>
2021-07-26 16:21:16 +0900

    ing

--- pipex/include/pipex.h
+++ pipex/include/pipex.h
@@ -6,7 +6,7 @@
 /*   By: minskim2 <minskim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/21 23:26:52 by minskim2          #+#    #+#             */
-/*   Updated: 2021/07/26 16:12:53 by minskim2         ###   ########.fr       */
+/*   Updated: 2021/07/26 16:17:00 by minskim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,8 +36,8 @@ size_t	ft_strlen(const char *str);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	**ft_split(char const *s, char c);
 
-void		redirect_in(const char *file);
-void		redirect_out(const char *file);
+void	redirect_in(const char *file);
+void	redirect_out(const char *file);
 
 # define CHILD 0
 
