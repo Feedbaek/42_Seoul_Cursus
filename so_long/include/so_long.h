e8780b1cff959d147068405732af2816152109e9
minskim2 <nicek789@naver.com>
2021-08-18 18:39:12 +0900

    ing

--- so_long/include/so_long.h
+++ so_long/include/so_long.h
@@ -6,7 +6,7 @@
 /*   By: minskim2 <minskim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/27 18:19:25 by minskim2          #+#    #+#             */
-/*   Updated: 2021/08/17 21:50:18 by minskim2         ###   ########.fr       */
+/*   Updated: 2021/08/18 13:25:13 by minskim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,10 +39,16 @@ typedef struct s_img
 {
 	void	*img_ptr;
 	int		*data;
-	int		*size_l;
+	int		size_l;
 	int		bpp;
 	int		endian;
 }	t_img;
+
+typedef struct s_mlx
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
+}	t_mlx;
 
 void	param_init(t_param *param);
 int		key_press(int key, t_param *param);
--- so_long/srcs/so_long.c
+++ so_long/srcs/so_long.c
@@ -6,7 +6,7 @@
 /*   By: minskim2 <minskim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/27 21:42:34 by minskim2          #+#    #+#             */
-/*   Updated: 2021/08/17 20:57:26 by minskim2         ###   ########.fr       */
+/*   Updated: 2021/08/18 13:34:08 by minskim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,34 @@
 
 int	main(void)
 {
-	void	*mlx_ptr;
-	void	*win_ptr;
-	void	*img_ptr;
+	t_mlx	mlx;
+	t_img	img;
 	t_param	param;
-	int		width;
-	int		height;
+	//int		width;
+	//int		height;
+	int		count_w;
+	int		count_h;
 
-	mlx_ptr = mlx_init();
+	mlx.mlx_ptr = mlx_init();
+	mlx.win_ptr = mlx_new_window(mlx.mlx_ptr, 800, 600, "mlx 42");
 	param_init(&param);
-	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "mlx 42");
-	img_ptr = mlx_xpm_file_to_image(mlx_ptr, "./img/test.xpm", &width, &height);
-	mlx_put_image_to_window(mlx_ptr, win_ptr, img_ptr, 0, 0);
-	mlx_hook(win_ptr, X_EVENT_KEY_PRESS, 0, &key_press, &param);
-	mlx_loop(mlx_ptr);
+	img.img_ptr = mlx_new_image(mlx.mlx_ptr, 400, 300);
+	img.data = (int *)mlx_get_data_addr(img.img_ptr, &img.bpp, &img.size_l, &img.endian);
+	//img.img_ptr = mlx_xpm_file_to_image(mlx.mlx_ptr, "./img/test.xpm", &width, &height);
+	count_h = -1;
+	while (++count_h < 300)
+	{
+		count_w = -1;
+		while (++count_w < 400)
+		{
+			if (count_w % 2)
+				img.data[count_h * 400 + count_w] = 0xFFFFFF;
+			else
+				img.data[count_h * 400 + count_w] = 0xFF0000;
+		}
+	}
+	mlx_put_image_to_window(mlx.mlx_ptr, mlx.win_ptr, img.img_ptr, 0, 0);
+	mlx_hook(mlx.win_ptr, X_EVENT_KEY_PRESS, 0, &key_press, &param);
+	mlx_loop(mlx.mlx_ptr);
 	return (0);
 }
