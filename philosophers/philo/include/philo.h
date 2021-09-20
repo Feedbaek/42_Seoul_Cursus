d47dd7dd941f77e7dbacb3d6c28270738e5886ae
minskim2 <nicek789@naver.com>
2021-09-21 01:53:20 +0900

    21.09.20

--- philosophers/philo/include/philo.h
+++ philosophers/philo/include/philo.h
@@ -6,7 +6,7 @@
 /*   By: minskim2 <minskim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/15 19:55:38 by minskim2          #+#    #+#             */
-/*   Updated: 2021/09/20 14:42:03 by minskim2         ###   ########.fr       */
+/*   Updated: 2021/09/21 01:28:19 by minskim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,18 @@
 # include <stdlib.h>
 # include <sys/time.h>
 # include <pthread.h>
+
+typedef struct s_philo
+{
+	int				philo_num;
+	int				time_die;
+	int				time_eat;
+	int				time_sleep;
+	int				time_opt;
+	pthread_t		*philos;
+	pthread_mutex_t	*forks;
+}	t_philo;
+
+int	ft_atoi(char *str);
 
 #endif
--- philosophers/philo/srcs/philo.c
+++ philosophers/philo/srcs/philo.c
@@ -6,16 +6,42 @@
 /*   By: minskim2 <minskim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/15 19:20:08 by minskim2          #+#    #+#             */
-/*   Updated: 2021/09/20 15:22:27 by minskim2         ###   ########.fr       */
+/*   Updated: 2021/09/21 01:34:29 by minskim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <philo.h>
 
-int	main(int argc, char *argv[])
+int	init_philo(t_philo *philo, int argc, char **argv)
 {
-	if (argc < 5 || argc > 6)
+	philo->philo_num = ft_atoi(argv[1]);
+	philo->time_die = ft_atoi(argv[2]);
+	philo->time_eat = ft_atoi(argv[3]);
+	philo->time_sleep = ft_atoi(argv[4]);
+	if (philo->philo_num < 1 || philo->time_die < 1 || philo->time_eat < 1 || philo->time_sleep < 1)
 		return (0);
+	if (argc == 6)
+	{
+		philo->time_opt = ft_atoi(argv[5]);
+		if (philo->time_opt < 1)
+			return (0);
+	}
+	else
+		philo->time_opt = 0;
+	philo->philos = (pthread_t *)malloc(sizeof(pthread_t) * philo->philo_num);
+	philo->forks = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t) * philo->philo_num);
+	if (!philo->philos || !philo->forks)
+		return (0);
+	return (1);
+}
 
+int	main(int argc, char **argv)
+{
+	t_philo	philo;
+
+	if (argc < 5 || argc > 6)
+		return (-1);
+	if (!init_philo(&philo, argc, argv))
+		return (-1);
 	return (0);
 }
