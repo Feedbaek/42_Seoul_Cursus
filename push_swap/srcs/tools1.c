30268cc44b561253ffa4b18a984e6dacd303ec95
minskim2 <nicek789@naver.com>
2021-09-21 00:34:48 +0900

    end

--- push_swap/srcs/tools1.c
+++ push_swap/srcs/tools1.c
@@ -6,7 +6,7 @@
 /*   By: minskim2 <minskim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/24 18:30:02 by minskim2          #+#    #+#             */
-/*   Updated: 2021/09/20 16:57:14 by minskim2         ###   ########.fr       */
+/*   Updated: 2021/09/21 00:33:14 by minskim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,5 +61,5 @@ int	ft_strncmp(char *s1, char *s2, size_t n)
 void	error_push_swap(char *error)
 {
 	write(2, error, ft_strlen(error));
-	exit(0);
+	exit(1);
 }
