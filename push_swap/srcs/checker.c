d34e81a6f04e8545572b4d1de3883bbe923182f3
Feedbaek <nicek789@naver.com>
2021-09-20 16:53:34 +0900

    end

--- push_swap/srcs/checker.c
+++ push_swap/srcs/checker.c
@@ -6,7 +6,7 @@
 /*   By: minskim2 <minskim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/06 15:04:56 by minskim2          #+#    #+#             */
-/*   Updated: 2021/09/20 16:51:33 by minskim2         ###   ########.fr       */
+/*   Updated: 2021/09/20 16:53:29 by minskim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,7 +45,7 @@ void	run_cmd(char *cmd, t_inform *inform)
 	}
 	else if (ft_strlen(cmd) == 3)
 		run_cmd_1(cmd, inform);
-	else
+	else if (ft_strlen(cmd) == 6)
 		error_push_swap("Error\n");
 }
 
