9fa2e0fe776fe19d0938fb6297ab06c5719d940c
minskim2 <nicek789@naver.com>
2022-04-03 22:46:51 +0900

    a

--- cpp_module/cpp_0/ex01/PhoneBook.hpp
+++ cpp_module/cpp_0/ex01/PhoneBook.hpp
@@ -6,7 +6,7 @@
 /*   By: minskim2 <minskim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/01 16:36:15 by minskim2          #+#    #+#             */
-/*   Updated: 2022/04/01 17:33:29 by minskim2         ###   ########.fr       */
+/*   Updated: 2022/04/03 22:45:58 by minskim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,13 @@
 
 class PhoneBook {
 private:
-	static Contact arr[8];
-
+	static Contact	*arr[8];
+	static int		input_cnt;
 public:
-
+	void	add(Contact *data) {
+		arr[input_cnt % 8] = data;
+		input_cnt++;
+	}
 };
 
 #endif
--- cpp_module/cpp_0/ex01/main.cpp
+++ cpp_module/cpp_0/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: minskim2 <minskim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/01 16:35:34 by minskim2          #+#    #+#             */
-/*   Updated: 2022/04/01 17:40:02 by minskim2         ###   ########.fr       */
+/*   Updated: 2022/04/03 22:46:20 by minskim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,40 @@
 #include "Contact.hpp"
 #include "PhoneBook.hpp"
 
-int	main(void) {
+void	ADD(PhoneBook &book) {
+	std::string f_name, l_name, n_name, p_num, d_secret;
+	Contact *c = new Contact();
+	std::cin >> f_name >> l_name >> n_name >> p_num >> d_secret;
+	c->contact_add(f_name, l_name, n_name, p_num, d_secret);
+	book.add(c);
+}
+
+std::string	convert(std::string str) {
+	std::string *s = new std::string();
+	for (int i=0; i<str.size(); i++) {
+		if (i == 9) {
+			if (str.size() > 10) {
+				*s += '.';
+				break;
+			}
+		}
+		*s += str[i];
+	}
+}
+
+void	SEARCH(PhoneBook &book) {
+	char	index[10];
+	std::string	s_index = convert(book)
+	char	f_name[10];
+	std::string	s_f_name(f_name);
+	char	l_name[10];
+	std::string	s_l_name(l_name);
+	char	n_name[10];
+	std::string	s_n_name(n_name);
+	std::cout << s_index+"|" << s_f_name+"|" << s_l_name+"|" << s_n_name;
+}
+
+int		main(void) {
+	PhoneBook book;
 	return (0);
 }
