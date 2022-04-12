752db0350690697b4496c613cd795814d7e97949
minskim2 <nicek789@naver.com>
2022-04-12 22:42:55 +0900

    ing

---
+++ cpp_module/cpp_02/ex00/Fixed.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: minskim2 <minskim2@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/12 22:31:17 by minskim2          #+#    #+#             */
+/*   Updated: 2022/04/12 22:41:16 by minskim2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+#include <iostream>
+
+class Fixed {
+private:
+	int	fixed_point;
+	static const int fractional_bits;
+public:
+	Fixed();
+	~Fixed();
+	Fixed(const Fixed &a);
+	Fixed	&operator= (const Fixed &a);
+	int		getRawBits(void) const;
+	void	setRawBits(int const raw);
+};
+
+#endif
