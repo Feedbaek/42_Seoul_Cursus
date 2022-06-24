4281deeb7d6a3a1b8843129b1ef2ccfb43360ab0
feedbaek <nicek789@naver.com>
2022-06-24 20:51:46 +0900

    map: red_black_tree 구현 시작

---
+++ ft_containers/red_black_tree.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   red_black_tree.hpp                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: minskim2 <minskim2@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/24 19:13:04 by minskim2          #+#    #+#             */
+/*   Updated: 2022/06/24 20:10:13 by minskim2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef RED_BLACK_TREE_HPP
+# define RED_BLACK_TREE_HPP
+
+# include <memory>
+
+# include "iterator.hpp"
+
+namespace ft {
+
+template<typename T, typename Compare>
+class red_black_tree {
+
+}
+
+}
+
+#endif
