a885e4fdb5d69410f03425ece9211155148df924
feedbaek <nicek789@naver.com>
2022-06-27 22:21:35 +0900

    map: node_tree 구현, tree_iterator 구현중

--- ft_containers/red_black_tree.hpp
+++ ft_containers/red_black_tree.hpp
@@ -6,7 +6,7 @@
 /*   By: minskim2 <minskim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/24 19:13:04 by minskim2          #+#    #+#             */
-/*   Updated: 2022/06/24 20:10:13 by minskim2         ###   ########.fr       */
+/*   Updated: 2022/06/27 22:20:39 by minskim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,65 @@
 # define RED_BLACK_TREE_HPP
 
 # include <memory>
+# include <cstddef>
 
 # include "iterator.hpp"
+# include "utils.hpp"
 
 namespace ft {
 
+template<typename T>
+class tree_iterator : public iterator<bidirectional_iterator_tag, T> {
+public:
+	typedef tree_node<T>	node;
+	typedef tree_node<T>*	node_pointer;
+
+	typedef bidirectional_iterator_tag	iterator_category;
+	typedef T							value_type;
+	typedef value_type&					reference;
+	typedef std::ptrdiff_t				difference_type;
+	typedef T*							pointer;
+
+private:
+	node_pointer _node;
+
+public:
+	tree_iterator() : _node(0) {}
+	tree_iterator(const tree_iterator& a) : _node(a._node) {}
+	tree_iterator(node_pointer a) : _node(a) {}
+
+	virtual ~tree_iterator() {}
+
+	tree_iterator& operator=(const tree_iterator& a) {
+		if (this == &a)
+			return *this;
+		_node = a._node;
+		return *this;
+	}
+	tree_iterator& operator++() {
+		if (_node->right != 0) {
+
+		}
+	}
+}
+
 template<typename T, typename Compare>
 class red_black_tree {
+public:
+	typedef T							value_type;
+	typedef Compare						value_compare;
+	typedef std::allocator<T>			allocator_type;
+	typedef size_t						size_type;
+	typedef std::ptrdiff_t				difference_type;
+	typedef typename allocator_type::reference			reference;
+	typedef typename allocator_type::const_reference	const_reference;
+	typedef typename allocator_type::pointer			pointer;
+	typedef typename allocator_type::const_pointer		const_pointer;
 
+	typedef red_black_tree				rbtree;
+	typedef tree_node<T>				node_type;
+	typedef tree_node<T>*				node_pointer;
+	typedef std::allocator<node_type>	node_alloc_type;
 }
 
 }
--- ft_containers/utils.hpp
+++ ft_containers/utils.hpp
@@ -6,7 +6,7 @@
 /*   By: minskim2 <minskim2@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/17 21:29:49 by minskim2          #+#    #+#             */
-/*   Updated: 2022/06/17 21:38:33 by minskim2         ###   ########.fr       */
+/*   Updated: 2022/06/27 22:01:30 by minskim2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,58 @@ typename iterator_traits<InputIterator>::difference_type distance(InputIterator 
 	}
 	for (;first != last; first++, diff++) {	}
 	return sign * diff;
+}
+
+enum Color {
+	RED,
+	BLACK,
+	DBLACK
+};
+
+template<typename T>
+struct tree_node {
+	typedef T		value_type;
+	typedef Color	color_type;
+
+	value_type		value;
+	tree_node*		parent;
+	tree_node*		left;
+	tree_node*		right;
+	color_type		color;
+
+	tree_node() : value(value_type()), parent(0), left(0), right(0), color(RED) {
+	}
+	tree_node(const value_type& val, tree_node* pr = 0, tree_node* lf = 0, tree_node* ri = 0, color_type col = RED)
+		: value(val), parent(pr), left(lf), right(ri), color(col) {
+	}
+	tree_node(const tree_node& a) : value(a.value), parent(a.parent), left(a.left), right(a.right), color(a.color) {
+	}
+
+	virtual ~tree_node() {}
+
+	tree_node& operator=(const tree_node& a) {
+		if (this == &a)
+			return (*this);
+		value = a.value;
+		parent = a.parent;
+		left = a.left;
+		right = a.right;
+		color = a.color;
+		return (*this);
+	}
+};
+
+template<typename T1, typename T2>
+bool operator==(const tree_node<T1>& lhs, const tree_node<T2>& rhs) {
+	if (lhs.value == rhs.value)
+		return true;
+	return false;
+}
+template<typename T1, typename T2>
+bool operator!=(const tree_node<T1>& lhs, const tree_node<T2>& rhs) {
+	if (lhs.value != rhs.value)
+		return true;
+	return false;
 }
 
 }
